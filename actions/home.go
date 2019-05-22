cff193f32c9da6f3c62e7acb4a301d81b639ee4f
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-05-22 07:44:39 -0500

    Integration of issues

--- actions/home.go
+++ actions/home.go
@@ -1,9 +1,37 @@
 package actions
 
-import "github.com/gobuffalo/buffalo"
+import (
+	"errors"
+
+	"issue_tracker/models"
+
+	"github.com/gobuffalo/buffalo"
+	"github.com/gobuffalo/pop"
+)
 
 // HomeHandler is a default handler to serve up
 // a home page.
 func HomeHandler(c buffalo.Context) error {
+	// Get the DB connection from the context
+	tx, ok := c.Value("tx").(*pop.Connection)
+	if !ok {
+		return errors.New("no transaction found")
+	}
+
+	issues := &models.Issues{}
+
+	// Paginate results. Params "page" and "per_page" control pagination.
+	// Default values are "page=1" and "per_page=20".
+	q := tx.PaginateFromParams(c.Params())
+
+	// Retrieve all Issues from the DB
+	if err := q.All(issues); err != nil {
+		return err
+	}
+
+	// Add the paginator to the context so it can be used in the template.
+	c.Set("pagination", q.Paginator)
+	c.Set("books", issues)
+
 	return c.Render(200, r.HTML("index.html"))
 }
